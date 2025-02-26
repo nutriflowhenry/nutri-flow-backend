d8bfa8875a27a6a272f915227e50ac0b7872f4a6
Omar <omaraguilalira2@gmail.com>
2025-02-26 09:31:16 -0600

    style: apply Prettier formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {UsersModule} from "./modules/users/users.module";
+import { UsersModule } from './modules/users/users.module';
+import { WaterTrackerModule } from './modules/water-tracker/water-tracker.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, WaterTrackerModule],
   controllers: [AppController],
   providers: [AppService],
 })
