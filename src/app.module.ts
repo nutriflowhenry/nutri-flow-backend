69b636b1e6d7e15d6f1b51d7b9b80d2b3e96d89c
Omar <omaraguilalira2@gmail.com>
2025-02-26 08:12:45 -0600

    TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {UsersModule} from "./modules/users/users.module";
+import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { WaterTrackerModule } from './modules/water-tracker/water-tracker.module';
+import typeOrmConfig from './config/typeOrm.config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    UsersModule,
+    WaterTrackerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeOrm.config.ts
@@ -0,0 +1,26 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({ path: '.env' });
+
+const typeOrmConfig = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  autoLoadEntities: true,
+  dropSchema: false,
+  synchronize: true,
+  logging: true,
+  // migrations: ['dist/migrations/*{.js,.ts}'],
+};
+
+export default registerAs('typeorm', () => typeOrmConfig);
+
+export const connnectionSource = new DataSource(
+  typeOrmConfig as DataSourceOptions,
+);
