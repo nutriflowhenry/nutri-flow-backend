48055b77898e0bc03f7b02e43018dc6986f2ceb6
Omar <omarguilalira2@gmail.com>
2025-03-01 12:42:38 -0600

    Modification of user_profile to connect it with watertracker and foodtracker

--- src/modules/user-profiles/user-profiles.controller.ts
+++ src/modules/user-profiles/user-profiles.controller.ts
@@ -1,36 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserProfilesService } from './user-profiles.service';
 import { CreateUserProfileDto } from './dto/create-user-profile.dto';
 import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
 
 @Controller('user-profiles')
 export class UserProfilesController {
-  constructor(private readonly userProfilesService: UserProfilesService) {
-  }
+  constructor(private readonly userProfilesService: UserProfilesService) {}
 
-  @Post()
-  create(@Body() createUserProfileDto: CreateUserProfileDto) {
-    return this.userProfilesService.create(createUserProfileDto);
-  }
+  // @Post()
+  // create(@Body() createUserProfileDto: CreateUserProfileDto) {
+  //   return this.userProfilesService.create(createUserProfileDto);
+  // }
 
-  @Get()
-  findAll() {
+  // @Get()
+  // findAll() {
 
-    return this.userProfilesService.findAll();
-  }
+  //   return this.userProfilesService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userProfilesService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userProfilesService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserProfileDto: UpdateUserProfileDto) {
-    return this.userProfilesService.update(+id, updateUserProfileDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserProfileDto: UpdateUserProfileDto) {
+  //   return this.userProfilesService.update(+id, updateUserProfileDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userProfilesService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userProfilesService.remove(+id);
+  // }
 }
