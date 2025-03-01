085ccf6ffcdcc7f99346d79270d387f4443a1515
Omar <omarguilalira2@gmail.com>
2025-03-01 12:50:42 -0600

    Functionality to get a user by ID.

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,23 +1,28 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { User } from "./entities/user.entity";
-import { Repository } from "typeorm";
-import { Injectable } from "@nestjs/common";
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersRepository {
-   constructor(@InjectRepository(User) private repository: Repository<User>) {
-   }
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
 
-   async findByEmail(email: string): Promise<User> {
-      return this.repository.findOneBy({ email })
-   }
+  async findByEmail(email: string): Promise<User> {
+    return this.repository.findOneBy({ email });
+  }
 
-   createUser(userData: Omit<CreateUserDto, 'passwordConfirmation'>): Promise<User> {
-      return this.repository.save(userData);
-   }
+  createUser(
+    userData: Omit<CreateUserDto, 'passwordConfirmation'>,
+  ): Promise<User> {
+    return this.repository.save(userData);
+  }
 
-   async findAll(): Promise<User[]> {
-      return this.repository.find();
-   }
+  async findAll(): Promise<User[]> {
+    return this.repository.find();
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.repository.findOneBy({ id });
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,33 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UsersRepository } from "./users.repository";
+import { UsersRepository } from './users.repository';
 import { plainToInstance } from 'class-transformer';
 import { PublicUserDto } from './dto/public-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-   constructor(private usersRepository: UsersRepository) {
-   }
+  constructor(private usersRepository: UsersRepository) {}
 
-   create(createUserDto: CreateUserDto) {
-      return 'This action adds a new user';
-   }
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
 
-   async findAll(): Promise<PublicUserDto[]> {
-      const users = await this.usersRepository.findAll();
-      return plainToInstance(PublicUserDto, users)
-   }
+  async findAll(): Promise<PublicUserDto[]> {
+    const users = await this.usersRepository.findAll();
+    return plainToInstance(PublicUserDto, users);
+  }
 
-   findOne(id: number) {
-      return `This action returns a #${id} user`;
-   }
+  async findById(id: string): Promise<User> {
+    return this.usersRepository.findById(id);
+  }
 
-   update(id: number, updateUserDto: UpdateUserDto) {
-      return `This action updates a #${id} user`;
-   }
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
 
-   remove(id: number) {
-      return `This action removes a #${id} user`;
-   }
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
