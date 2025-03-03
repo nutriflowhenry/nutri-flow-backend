fd0beec9e381d249b690d63a3920f2d209ee373e
karen.c <karencasallas.arq@gmail.com>
2025-03-03 10:29:09 -0500

    cors

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -6,11 +6,11 @@ dotenvConfig({ path: '.env' });
 
 const typeOrmConfig = {
    type: 'postgres',
-   host: process.env.DB_HOST,
-   port: parseInt(process.env.DB_PORT, 10),
-   username: process.env.DB_USERNAME,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
+   host: 'localhost',
+   port: parseInt('5432', 10),
+   username: 'postgres',
+   password: '1234',
+   database: 'nutriflow',
    entities: ['dist/**/*.entity{.ts,.js}'],
    autoLoadEntities: true,
    dropSchema: false,
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors({
+    origin: "http://localhost:3001",
+    credentials: true, 
+    allowedHeaders: "Content-Type, Authorization",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+  });
+
   await app.listen(3000);
 }
 bootstrap();
