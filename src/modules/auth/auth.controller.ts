7893fd5dcb424f92f5fd39b321c3876b9a2125e4
Omar <omarguilalira2@gmail.com>
2025-03-03 22:28:59 -0600

    commit @nestjs/passport

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,50 +1,53 @@
 import {
-   BadRequestException,
-   Body,
-   Controller,
-   Get,
-   HttpCode,
-   HttpStatus,
-   Post,
-   Query,
-   Req,
-   UseGuards
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateLocalUserDto } from '../users/dto/create-local-user.dto';
 import { LoginUserDto } from '../users/dto/login-user.dto';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-   constructor(private readonly authService: AuthService) {
-   }
+  constructor(private readonly authService: AuthService) {}
 
-   @Post('signup')
-   @HttpCode(HttpStatus.CREATED)
-   signUp(@Body() userData: CreateLocalUserDto) {
-      console.log(userData);
-      return this.authService.signUp(userData);
-   }
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  signUp(@Body() userData: CreateLocalUserDto) {
+    console.log(userData);
+    return this.authService.signUp(userData);
+  }
 
-   @Post('login')
-   @HttpCode(HttpStatus.OK)
-   login(@Body() credentials: LoginUserDto) {
-      return this.authService.logIn(credentials);
-   }
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() credentials: LoginUserDto) {
+    return this.authService.logIn(credentials);
+  }
 
-   @Get('auth0/callback')
-   async auth0Login(@Query('code') code: string) {
-      if (!code) throw new BadRequestException('Authorization code is required');
+  @Get('auth0/callback')
+  async auth0Login(@Query('code') code: string) {
+    if (!code) throw new BadRequestException('Authorization code is required');
 
-      // Exchange code for Auth0 access token
-      console.log('Authorization code:', code);
-      const { access_token } = await this.authService.exchangeCodeForToken(code);
+    // Exchange code for Auth0 access token
+    console.log('Authorization code:', code);
+    const { access_token } = await this.authService.exchangeCodeForToken(code);
 
-      //  Fetch user profile from Auth0
-      const auth0User = await this.authService.fetchUserProfile(access_token);
+    //  Fetch user profile from Auth0
+    const auth0User = await this.authService.fetchUserProfile(access_token);
 
-      // Validate and issue JWT
-      return this.authService.validateAuth0User(auth0User.email, auth0User.sub, auth0User.name);
-   }
+    // Validate and issue JWT
+    return this.authService.validateAuth0User(
+      auth0User.email,
+      auth0User.sub,
+      auth0User.name,
+    );
+  }
 }
