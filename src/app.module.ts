7d98b2c9d3cf4718b2a3d859b7a1a442643be9ef
RenzoJimenezB <adrianrjb@outlook.es>
2025-03-07 14:44:52 -0500

    S3Module implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,29 +9,42 @@ import { FoodTrackerModule } from './modules/food-tracker/food-tracker.module';
 import typeOrmConfig from './config/typeOrm.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { S3Module } from 'nestjs-s3';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [typeOrmConfig],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        configService.get('typeorm'),
-    }),
-    AuthModule,
-    UsersModule,
-    WaterTrackerModule,
-    FoodTrackerModule,
-    JwtModule.register({
-      global: true,
-      signOptions: { expiresIn: '1h' },
-      secret: process.env.JWT_SECRET,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [typeOrmConfig],
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) =>
+                configService.get('typeorm'),
+        }),
+        JwtModule.register({
+            global: true,
+            signOptions: { expiresIn: '1h' },
+            secret: process.env.JWT_SECRET,
+        }),
+        S3Module.forRootAsync({
+            useFactory: () => ({
+                config: {
+                    credentials: {
+                        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                    },
+                    region: process.env.AWS_REGION,
+                }
+            })
+        }),
+        AuthModule,
+        UsersModule,
+        WaterTrackerModule,
+        FoodTrackerModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
