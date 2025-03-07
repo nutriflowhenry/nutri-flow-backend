06180ac2f34ca823cc3f586d32fc27ed4adf615c
RenzoJimenezB <adrianrjb@outlook.es>
2025-03-07 14:47:37 -0500

    images.controller.ts

---
+++ src/modules/images/images.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { CloudFrontService } from './aws/CloudFront.service';
+import { FoodTrackerService } from '../food-tracker/food-tracker.service';
+
+@Controller('upload')
+export class ImagesController {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly cloudFrontService: CloudFrontService,
+        private readonly foodTrackerService: FoodTrackerService) {
+    }
+
+    @Get('profile/upload-url/:userId')
+    getProfileUploadUrl(
+        @Param('userId') userId: string,
+        @Query('fileType') fileType: string): Promise<string> {
+        return this.usersService.getImageUploadUrl(userId, fileType);
+    }
+
+
+    @Get('meal/upload-url/:foodTrackerId')
+    getMealUploadUrl(
+        @Param('foodTrackerId') foodTrackerId: string,
+        @Query('fileType') fileType: string,): Promise<string> {
+        return this.foodTrackerService.getImageUploadUrl(foodTrackerId, fileType);
+    }
+
+}
