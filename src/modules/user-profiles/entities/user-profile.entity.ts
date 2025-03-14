148ab5808aee1cdd391e784cc8a2a95cc3b36b0c
Omar <omarguilalira2@gmail.com>
2025-03-14 12:49:20 -0600

    generation of resources for calculating water and calorie consumption

--- src/modules/user-profiles/entities/user-profile.entity.ts
+++ src/modules/user-profiles/entities/user-profile.entity.ts
@@ -1,9 +1,9 @@
 import {
-    Column,
-    Entity,
-    OneToMany,
-    OneToOne,
-    PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 import { Gender } from '../enums/gender.enum';
@@ -12,40 +12,50 @@ import { Gender } from '../enums/gender.enum';
 // import { DietType } from '../enums/diet-type.enum';
 import { FoodTracker } from 'src/modules/food-tracker/entities/food-tracker.entity';
 import { WaterTracker } from 'src/modules/water-tracker/entities/water-tracker.entity';
+import { ActivityLevel } from '../enums/activity-level.enum';
+import { Goal } from '../enums/goal.enum';
 
 @Entity()
 export class UserProfile {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
+  @OneToOne(() => User, (user) => user.userProfile, { onDelete: 'CASCADE' })
+  user: User;
 
-    @OneToOne(() => User, (user) => user.userProfile, { onDelete: 'CASCADE' })
-    user: User;
+  @OneToMany(() => WaterTracker, (waterTracker) => waterTracker.userProfile)
+  waterTrackers: WaterTracker[];
 
-    @OneToMany(() => WaterTracker, (waterTracker) => waterTracker.userProfile)
-    waterTrackers: WaterTracker[];
+  @OneToMany(() => FoodTracker, (foodTracker) => foodTracker.userProfile)
+  foodTrackers: FoodTracker[];
 
-    @OneToMany(() => FoodTracker, (foodTracker) => foodTracker.userProfile)
-    foodTrackers: FoodTracker[];
+  @Column({ type: 'date', nullable: false })
+  birthdate: Date;
 
-    @Column({ type: 'date', nullable: false })
-    birthdate: Date;
+  @Column({ type: 'enum', enum: Gender })
+  gender: Gender;
 
-    @Column({ type: 'enum', enum: Gender })
-    gender: string;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: false })
+  weight: number;
 
-    @Column({ type: 'decimal', precision: 5, scale: 2, nullable: false })
-    weight: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: false })
+  height: number;
 
-    @Column({ type: 'decimal', precision: 5, scale: 2, nullable: false })
-    height: number;
+  @Column({ type: 'enum', enum: ActivityLevel, nullable: true })
+  activityLevel: ActivityLevel;
 
-    // @Column({ type: 'enum', enum: ActivityLevel, nullable: false })
-    // activityLevel: ActivityLevel;
+  @Column({ type: 'enum', enum: Goal, nullable: true })
+  weightGoal: Goal;
 
-    // @Column({ type: 'enum', enum: Goal, nullable: false })
-    // goal: Goal;
+  @Column({ type: 'integer', default: 0 })
+  age: number;
 
-    // @Column({ type: 'enum', enum: DietType, default: DietType.NONE })
-    // dietType: string;
+  @Column({ type: 'integer', default: 0 })
+  caloriesGoal: number;
+
+  @Column({ type: 'integer', default: 0 })
+  hydrationGoal: number;
+
+  // @Column({ type: 'enum', enum: DietType, default: DietType.NONE })
+  // dietType: string;
 }
