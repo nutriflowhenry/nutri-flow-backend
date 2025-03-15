dcadddc24e440c141510b5f80c2f7a87e017730e
Omar <omarguilalira2@gmail.com>
2025-03-15 16:43:59 -0600

    fix google registered users now receives email

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,31 +5,26 @@ import { LoginUserDto } from '../users/dto/login-user.dto';
 import { PublicUserDto } from '../users/dto/public-user.dto';
 import { ApiTags } from '@nestjs/swagger';
 
-
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-    constructor(private readonly authService: AuthService) {
-    }
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  signUp(@Body() userData: CreateLocalUserDto): Promise<PublicUserDto> {
+    console.log(userData);
+    return this.authService.signUp(userData);
+  }
 
-    @Post('signup')
-    @HttpCode(HttpStatus.CREATED)
-    signUp(@Body() userData: CreateLocalUserDto): Promise<PublicUserDto> {
-        console.log(userData);
-        return this.authService.signUp(userData);
-    }
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() credentials: LoginUserDto) {
+    return this.authService.logIn(credentials);
+  }
 
-
-    @Post('login')
-    @HttpCode(HttpStatus.OK)
-    login(@Body() credentials: LoginUserDto) {
-        return this.authService.logIn(credentials);
-    }
-
-
-    @Post('google')
-    googleAuth(@Body('token') token: string) {
-        return this.authService.authenticateWithGoogle(token);
-    }
+  @Post('google')
+  googleAuth(@Body('token') token: string) {
+    return this.authService.authenticateWithGoogle(token);
+  }
 }
