65d15d5ed6f9b8d7ac66e93a69bdbcb301f34342
RenzoJimenezB <adrianrjb@outlook.es>
2025-03-18 12:05:14 -0500

    refactor and unban endpoint

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -5,22 +5,22 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 dotenvConfig({ path: '.env' });
 
 const typeOrmConfig = {
-   type: 'postgres',
-   host: process.env.DB_HOST,
-   port: parseInt(process.env.DB_PORT, 10),
-   username: process.env.DB_USERNAME,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
-   entities: ['dist/**/*.entity{.ts,.js}'],
-   autoLoadEntities: true,
-   dropSchema: false,
-   synchronize: true,
-   logging: false,
-   // migrations: ['dist/migrations/*{.js,.ts}'],
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    autoLoadEntities: true,
+    dropSchema: false,
+    synchronize: true,
+    logging: false,
+    // migrations: ['dist/migrations/*{.js,.ts}'],
 };
 
 export default registerAs('typeorm', () => typeOrmConfig);
 
 export const connectionSource = new DataSource(
-   typeOrmConfig as DataSourceOptions,
+    typeOrmConfig as DataSourceOptions,
 );
