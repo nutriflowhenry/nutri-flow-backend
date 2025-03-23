fd179881b92e181ea086666343e599b34e15f4a3
RenzoJimenezB <adrianrjb@outlook.es>
2025-03-22 22:54:20 -0500

    twilio implementation

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -13,32 +13,32 @@ export class NotificationsService {
         private readonly usersService: UsersService) {
     }
 
-    @Cron(CronExpression.EVERY_HOUR)
-    async handleDailyMorningNotifications() {
-        console.log('Sending morning notifications...');
+    // @Cron(CronExpression.EVERY_HOUR)
+    // async handleDailyMorningNotifications() {
+    //     console.log('Sending morning notifications...');
+    //
+    //     const usersToNotify = await this.usersService.findAllUsersWithNotificationsEnabled();
+    //
+    //     for (const user of usersToNotify) {
+    //         const message = `Hola, ${user.name}! No olvides mantenerte hidratado hoy`;
+    //         const userLocalHour = DateTime.now().setZone(user.timeZone).hour;
+    //
+    //         if (userLocalHour === 9) {
+    //             await this.smsService.sendSms(user.phone, message);
+    //         }
+    //     }
+    // }
 
-        const usersToNotify = await this.usersService.findAllUsersWithNotificationsEnabled();
 
-        for (const user of usersToNotify) {
-            const message = `Hola, ${user.name}! No olvides mantenerte hidratado hoy 💧`;
-            const userLocalHour = DateTime.now().setZone(user.timeZone).hour;
-
-            if (userLocalHour === 9) {
-                await this.smsService.sendSms(user.phone, message);
-            }
-        }
-    }
-
-
-    @Cron(CronExpression.EVERY_MINUTE)
-    async handleAfternoonNotifications() {
-        console.log('Sending afternoon notifications...');
-
-        const usersToNotify = await this.usersService.findAllUsersWithNotificationsEnabled();
-
-        for (const user of usersToNotify) {
-            const message = `Hola, ${user.name}! No olvides mantenerte hidratado hoy 💧`;
-            await this.smsService.sendSms(user.phone, message);
-        }
-    }
+    // @Cron(CronExpression.EVERY_MINUTE)
+    // async handleAfternoonNotifications() {
+    //     console.log('Sending afternoon notifications...');
+    //
+    //     const usersToNotify = await this.usersService.findAllUsersWithNotificationsEnabled();
+    //
+    //     for (const user of usersToNotify) {
+    //         const message = `Hola, ${user.name}! No olvides mantenerte hidratado hoy :)`;
+    //         await this.smsService.sendSms(user.phone, message);
+    //     }
+    // }
 }
--- src/modules/notifications/sms.service.ts
+++ src/modules/notifications/sms.service.ts
@@ -1,34 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
+import { Twilio } from 'twilio';
 
 
 @Injectable()
 export class SmsService {
 
     private snsClient: SNSClient;
+    private twilioClient: Twilio;
 
     constructor() {
-        this.snsClient = new SNSClient({
-            region: process.env.AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            }
-        });
+        this.twilioClient = new Twilio(
+            process.env.TWILIO_ACCOUNT_SID,
+            process.env.TWILIO_AUTH_TOKEN,
+        );
+        // this.snsClient = new SNSClient({
+        //     region: process.env.AWS_REGION,
+        //     credentials: {
+        //         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        //         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        //     }
+        // });
     }
 
     async sendSms(phoneNumber: string, message: string): Promise<void> {
         try {
-            const params = {
-                Message: message,
-                PhoneNumber: phoneNumber,
-            };
+            const result = this.twilioClient.messages.create({
+                body: message,
+                from: process.env.TWILIO_PHONE_NUMBER,
+                to: phoneNumber,
+            });
 
-            const result = await this.snsClient.send(new PublishCommand(params));
-            console.log(`SMS sent to ${phoneNumber}. Message ID: ${result.MessageId}`);
+            console.log(`SMS sent successfully to ${phoneNumber}`);
 
         } catch (error) {
-            console.error(`Failed to send SMS:`, error);
+            console.error(`Failed to send SMS to ${phoneNumber}:`, error);
+            throw new Error('SMS sending failed');
         }
+
+
+        // try {
+        //     const params = {
+        //         Message: message,
+        //         PhoneNumber: phoneNumber,
+        //     };
+        //
+        //     const result = await this.snsClient.send(new PublishCommand(params));
+        //     console.log(`SMS sent to ${phoneNumber}. Message ID: ${result.MessageId}`);
+        //
+        // } catch (error) {
+        //     console.error(`Failed to send SMS:`, error);
+        // }
     }
 }
