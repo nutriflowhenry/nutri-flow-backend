eb47b8f1123962f29963da37ccd94050b31c3506
Omar <omaraguilalira2@gmail.com>
2025-05-18 17:10:07 -0600

    Update AWS connection configuration

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -29,7 +29,7 @@ export class NotificationsService {
   //     }
   // }
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  // @Cron(CronExpression.EVERY_MINUTE)
   async handleAfternoonNotifications() {
     console.log('Sending afternoon notifications...');
 
